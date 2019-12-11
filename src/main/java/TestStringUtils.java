1bd4dafcc2c2f15d0015b8ea0d4e9c786fe8b5bc
BeGear <BeGear@DESKTOP-II1Q4H7.station>
2019-12-11 09:39:46 +0100

    Prima creazione

---
+++ src/main/java/TestStringUtils.java
@@ -0,0 +1,10 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class TestStringUtils {
+	
+	public static void main(String[] args) {
+		String s1 = "12AA34";
+		System.out.println(StringUtils.isNumeric(s1));
+	}
+
+}
